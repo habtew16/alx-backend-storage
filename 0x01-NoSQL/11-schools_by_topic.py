03d8cc199ca61a5d9bb7b7399daf98375f7c8d1d
habtew16 <habtamuwolde96@gmail.com>
2024-05-04 19:23:17 +0000

    school by topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+task 11
+'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''
+    returns the list of school having a specific topic
+    '''
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
