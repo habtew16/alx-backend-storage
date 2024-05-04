1f0965717ae02681e1eaf3bdf8a30c133e4e8676
habtew16 <habtamuwolde96@gmail.com>
2024-05-04 19:19:43 +0000

    insert data to db

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+insert module
+'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''
+    inserts a new document in a collection
+    '''
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
